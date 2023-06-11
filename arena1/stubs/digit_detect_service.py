e222b05b61b822dfe9979428341293c0d3474bc8
wrjgold <125972533+wrjgold@users.noreply.github.com>
2023-06-11 23:22:50 +0800

    Add files via upload

---
+++ arena1/stubs/digit_detect_service.py
@@ -0,0 +1,32 @@
+# This is where you place the ASR codes
+from abstract_ai_services import AbstractDigitDetectionService, AbstractSpeakerIDService, AbstractObjectReIDService
+from tilsdk.cv.types import *
+from typing import Tuple, List
+import numpy as np
+
+
+class DigitDetectionService():
+    '''Implementation of the Digit Detection Service based on Automatic Speech Recognition.
+    '''
+    def __init__(self, model_dir:str):
+        '''
+        Parameters
+        ----------
+        model_dir : str
+            Path of model file to load.
+        '''
+        pass
+
+    def transcribe_audio_to_digits(self, audio_waveform: np.array) -> Tuple[int]:
+        '''Transcribe audio waveform to a tuple of ints.
+        
+        Parameters
+        ----------
+        audio_waveform : numpy.array
+            Numpy array of floats that represent the audio file. It is assumed that the sampling rate of the audio is 16K.
+        Returns
+        -------
+        results  :
+            The ordered tuple of digits found in the input audio file.
+        '''
+        return (1,2)  # mock value
